680da45c21b6a8e397b8604cd3f5629f90169b5b
Sanzhar <sanzhar.aubakirov@innoforce.kz>
2017-02-17 12:03:01 +0600

    TMP

--- app/src/main/java/kz/kaznu/bluelock/MainActivity.java
+++ app/src/main/java/kz/kaznu/bluelock/MainActivity.java
@@ -9,40 +9,57 @@ import android.bluetooth.BluetoothGattService;
 import android.bluetooth.BluetoothProfile;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.CompoundButton;
+import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.Switch;
 import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+
+import static java.util.concurrent.TimeUnit.SECONDS;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final long SCAN_PERIOD = 10 * 1000;
+    private static final long SCAN_PERIOD = 3 * 1000;
 
     private Switch bluePay_scan_Switch;
     private ProgressBar progressBar;
     private RecyclerView recyclerView;
     private BlueAdapter adapter;
     private List<BluePayDevice> devices;
+    private Button submitButton;
+    private EditText phone;
 
     private BluetoothAdapter mBluetoothAdapter;
     private BluetoothGatt mBluetoothGatt;
     private BluetoothAdapter.LeScanCallback mLeScanCallback;
+    private BluetoothDevice device = null;
 
     private Handler mHandler;
 
     private Boolean mScanning = false;
 
+    private static final String TO_WRITE = "00003a51";
+    private static final String TO_READ = "00003a52";
+    public static final String SERVICE = "0000183a";
+
+    private final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,9 +72,7 @@ public class MainActivity extends AppCompatActivity {
 
         // activity element configs
         configureActivityElements();
-
-        Set<BluePayDevice> devices = Utils.loadDevicesToDB(getApplicationContext());
-        System.out.println(devices);
+        Utils.saveDevicesToDB(getApplicationContext(), new HashSet<BluePayDevice>());
     }
 
     // Init of bluetooth shit
@@ -76,7 +91,7 @@ public class MainActivity extends AppCompatActivity {
 
                 if (device.getName() == null) return;
 
-                if (device.getName().toLowerCase().startsWith("preved")) {
+                if (device.getName().toLowerCase().startsWith("bluepay")) {
 
                     final String[] complexName = device.getName().split("_");
                     final String name = complexName[0];
@@ -104,6 +119,45 @@ public class MainActivity extends AppCompatActivity {
      * Здесь прописаны настройки элементов на экране activity_main
      */
     private void configureActivityElements() {
+
+        phone = (EditText) findViewById(R.id.phone);
+
+        submitButton = (Button) findViewById(R.id.submitButton);
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final List<BluetoothGattService> services = mBluetoothGatt.getServices();
+                for (BluetoothGattService service : services) {
+
+                    final List<BluetoothGattCharacteristic> characteristics = service.getCharacteristics();
+                    final String servUUID = service.getUuid().toString();
+
+                    if (servUUID.startsWith(SERVICE)) {
+                        Log.d("SERVICE", "+++++++++++++++++++++++++++++");
+                        Log.d("SERVICE_UUID", servUUID);
+
+                        for (BluetoothGattCharacteristic characteristic : characteristics) {
+
+                            final String charUUID = characteristic.getUuid().toString();
+
+                            //0x2A25
+                            Log.d("CHARACTERISTIC_UUID", charUUID);
+
+                            if (charUUID.startsWith(TO_WRITE)) {
+//                                mBluetoothGatt.readCharacteristic(characteristic);
+                                Log.d("START_WRITE", charUUID);
+                                characteristic.setValue(phone.getText().toString());
+                                mBluetoothGatt.beginReliableWrite();
+                                mBluetoothGatt.writeCharacteristic(characteristic);
+                            }
+
+                        }
+                        Log.d("SERVICE", "-----------------------------");
+                    }
+                }
+            }
+        });
+
         devices = new ArrayList<>(Utils.loadDevicesToDB(getApplicationContext()));
 
         adapter = new BlueAdapter(getApplicationContext(), devices);
@@ -137,7 +191,6 @@ public class MainActivity extends AppCompatActivity {
                             Toast.makeText(MainActivity.this, "Start connection",
                                     Toast.LENGTH_SHORT).show();
                             doConnectToClosestDevice();
-                            progressBar.setVisibility(View.INVISIBLE);
                         }
                     }
                 });
@@ -163,7 +216,6 @@ public class MainActivity extends AppCompatActivity {
         mScanning = false;
         mBluetoothAdapter.stopLeScan(mLeScanCallback);
         bluePay_scan_Switch.setChecked(false);
-        progressBar.setVisibility(View.INVISIBLE);
     }
 
     private void doConnectToClosestDevice() {
@@ -172,6 +224,7 @@ public class MainActivity extends AppCompatActivity {
         if (devices.isEmpty()) {
             Log.d("ACTION_FINISHED", "Device list is empty!");
             Toast.makeText(this, "DEVICES NOT FOUND", Toast.LENGTH_SHORT).show();
+            progressBar.setVisibility(View.INVISIBLE);
             return;
         }
 
@@ -179,15 +232,16 @@ public class MainActivity extends AppCompatActivity {
         Collections.sort(listOfAllDevices);
 
         final BluePayDevice bestConnectionCandidate = listOfAllDevices.get(0);
-        final BluetoothDevice best_device = bestConnectionCandidate.getDevice();
-        Log.d("BEST_DEVICE", best_device.getName());
+        final BluetoothDevice bestDevice = bestConnectionCandidate.getDevice();
+        Log.d("BEST_DEVICE", bestDevice.getName());
         Log.d("BEST_DEVICE_RSSI", String.valueOf(bestConnectionCandidate.getRssi()));
 
-        final BluetoothDevice device =
-                mBluetoothAdapter.getRemoteDevice(best_device.getAddress());
+        device = mBluetoothAdapter.getRemoteDevice(bestDevice.getAddress());
 
         mBluetoothGatt = device.connectGatt(getApplication(), true, mGattCallback);
         Log.d("BEST_DEVICE", "\n\tConnected to GATT server\n");
+        progressBar.setVisibility(View.INVISIBLE);
+        submitButton.setVisibility(View.VISIBLE);
     }
 
     private void traceDiscoveredServices() {
@@ -198,33 +252,36 @@ public class MainActivity extends AppCompatActivity {
             final List<BluetoothGattCharacteristic> characteristics = service.getCharacteristics();
             final String servUUID = service.getUuid().toString();
 
-            Log.d("SERVICE", "+++++++++++++++++++++++++++++");
-            Log.d("SERVICE_UUID", servUUID);
+            if (servUUID.startsWith("0x183A")) {
+                Log.d("SERVICE", "+++++++++++++++++++++++++++++");
+                Log.d("SERVICE_UUID", servUUID);
 
-            for (BluetoothGattCharacteristic characteristic : characteristics) {
-
-                final String charUUID = characteristic.getUuid().toString();
-
-                //0x2A25
-                Log.d("CHARACTERISTIC_UUID", charUUID);
-
-                if (charUUID.startsWith("00002a00")) {
-                    mBluetoothGatt.readCharacteristic(characteristic);
+                for (BluetoothGattCharacteristic characteristic : characteristics) {
+                    final String charUUID = characteristic.getUuid().toString();
+                    Log.d("CHARACTERISTIC_UUID", charUUID);
                 }
+                Log.d("SERVICE", "-----------------------------");
             }
-            Log.d("SERVICE", "-----------------------------");
         }
     }
 
-
-    // Implements callback methods for GATT events that the app cares about.  For example,
-    // connection change and services discovered.
     private final BluetoothGattCallback mGattCallback = new BluetoothGattCallback() {
+
+        @Override
+        public void onReadRemoteRssi(BluetoothGatt gatt, int rssi, int status) {
+            Log.d("RSSI", String.valueOf(rssi));
+            super.onReadRemoteRssi(gatt, rssi, status);
+        }
+
         @Override
         public void onConnectionStateChange(BluetoothGatt gatt, int status, int newState) {
             if (newState == BluetoothProfile.STATE_CONNECTED) {
                 Log.d("GATT_CONNECTION", "Connected to GATT server.");
-                mBluetoothGatt.discoverServices();
+
+                if (device != null && gatt.getDevice().equals(device)) {
+                    Log.d("GATT_DISCOVERY", "STARTED");
+                    mBluetoothGatt.discoverServices();
+                }
             } else if (newState == BluetoothProfile.STATE_DISCONNECTED) {
                 Log.d("GATT_DISCONNECTION", "Disconnected from GATT server.");
             }
@@ -232,9 +289,10 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public void onServicesDiscovered(BluetoothGatt gatt, int status) {
+            Log.d("GATT_DISCOVERY", "FINISHED " + status);
+
             if (status == BluetoothGatt.GATT_SUCCESS) {
-                Log.d("GATT_DISCOVERY", "onServicesDiscovered: " + status);
-                traceDiscoveredServices();
+//                traceDiscoveredServices();
             }
         }
 
@@ -242,12 +300,13 @@ public class MainActivity extends AppCompatActivity {
         public void onCharacteristicRead(BluetoothGatt gatt,
                                          BluetoothGattCharacteristic characteristic,
                                          int status) {
-            if (characteristic.getUuid().toString().startsWith("00002a00")) {
-                Log.d("INFO_SERIAL_NUMBER", new String(characteristic.getValue()));
+            final String msg = new String(characteristic.getValue());
 
-                characteristic.setValue("PREVED MEDVED!");
-                mBluetoothGatt.beginReliableWrite();
-                mBluetoothGatt.writeCharacteristic(characteristic);
+           Log.d("START_READ", msg);
+            if (characteristic.getUuid().toString().startsWith(TO_READ)) {
+                Log.d("START_READ", msg);
+
+                Toast.makeText(MainActivity.this, msg, Toast.LENGTH_LONG).show();
 
             }
             super.onCharacteristicRead(gatt, characteristic, status);
@@ -262,13 +321,35 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void onCharacteristicWrite(BluetoothGatt gatt,
                                           BluetoothGattCharacteristic characteristic, int status) {
-            Log.d("CHAR_WRITE_CALLBACK", new String(characteristic.getValue()));
 
-            if (characteristic.getUuid().toString().startsWith("00002a00")) {
-                Log.d("INFO_SERIAL_NUMBER", new String(characteristic.getValue()));
+            if (characteristic.getUuid().toString().startsWith(TO_WRITE)) {
+                Log.d("FINISH_WRITE", new String(characteristic.getValue()));
                 mBluetoothGatt.executeReliableWrite();
+
+                runOneShotTask(characteristic);
+
             }
+
             super.onCharacteristicWrite(gatt, characteristic, status);
         }
     };
+
+
+    public ScheduledFuture<?> runOneShotTask(final BluetoothGattCharacteristic characteristic) {
+
+        final ScheduledFuture<?> future = scheduler.schedule(getCommand(characteristic) , 3, SECONDS );
+
+        return future;
+
+    }
+
+    @NonNull
+    private Runnable getCommand(final BluetoothGattCharacteristic characteristic) {
+        return new Runnable() {
+            @Override
+            public void run() {
+                mBluetoothGatt.readCharacteristic(characteristic);
+            }
+        };
+    }
 }
